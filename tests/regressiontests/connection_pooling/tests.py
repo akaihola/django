80012ce7fb3a8272d022a9e753a7b5bfbf356a11
Brian Rosner <brosner@gmail.com>
2008-12-30 01:42:45 -0700

    Added the basis of the tests for connection pooling.

---
+++ tests/regressiontests/connection_pooling/tests.py
@@ -0,0 +1,23 @@
+
+import unittest
+import threading
+
+class ThreadLocalPoolTestCase(unittest.TestCase):
+    def test_thread_singleton_connections(self):
+        connections = set()
+        def runner():
+            from django.db import connection
+            # calling connection.cursor() will create a new connection, if
+            # one is not already present
+            connection.cursor()
+            connections.add(connection.pool.get())
+        threads = []
+        for x in xrange(2):
+            t = threading.Thread(target=runner)
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join(1)
+        # ensure all connections were unique
+        self.assertEquals(len(connections), 2)
+        
